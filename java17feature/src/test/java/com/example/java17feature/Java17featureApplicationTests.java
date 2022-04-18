810a287d02d715420c7538a95282b2a04646ff22
anicetkeric <anicetkeric@outlook.com>
2022-04-18 22:32:33 +0200

    initial commit

---
+++ java17feature/src/test/java/com/example/java17feature/Java17featureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java17feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17featureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ java17feature/src/test/java/com/example/java17feature/TextBlocksTest.java
@@ -0,0 +1,69 @@
+package com.example.java17feature;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TextBlocksTest {
+
+
+
+    @Test
+    void textBlocksJsonString() {
+        String text = """
+            {
+              "id": 45,
+              "enterprise": "SOPRA",
+              "country": "Espagne"
+            }
+            """;
+        System.out.println(text);
+        assertThat(text).isNotEmpty();
+    }
+    @Test
+    void textBlocksHtmlString() {
+        String html = """
+              <html>
+                  <body>
+                      <p>Hello, CDS</p>
+                  </body>
+              </html>
+              """;
+        System.out.println(html);
+        assertThat(html).isNotEmpty();
+    }
+
+    @Test
+    void textBlocksSqlString() {
+        String query = """
+               SELECT "EMP_ID", "LAST_NAME" FROM "EMPLOYEE_TB"
+               WHERE "CITY" = 'INDIANAPOLIS'
+               ORDER BY "EMP_ID", "LAST_NAME";
+               """;
+        System.out.println(query);
+        assertThat(query).isNotEmpty();
+    }
+
+    @Test
+    void textBlocksFunctionString() {
+        String function = """
+                         function hello() {
+                             print('"Hello, CDS"');
+                         }
+                         
+                         hello();
+                         """;
+        System.out.println(function);
+        assertThat(function).isNotEmpty();
+    }
+
+    @Test
+    void textBlocksSingleLineString() {
+        String singleLine = """
+          Hello \
+          World
+          """;
+        System.out.println(singleLine);
+        assertThat(singleLine).isNotEmpty();
+    }
+}
